9a6ce8a87bd0d7414edae797d15eeba723cfe95b
Seiji Villafranca <seiji.ralphvillafranca@kpn.com>
2023-10-04 15:57:13 +0200

    Added base components for controls and skeleton - added bolierplate code - registered as a custom element

---
+++ src/app/loaders/aphskeleton/aphskeleton.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aphskeleton',
+  standalone: true,
+  template: ``,
+  styles: [
+    `
+    `
+  ]
+})
+export class APHSkeletonComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { createCustomElement } from '@angular/elements';
 import { createApplication } from '@angular/platform-browser';
 import { APHButtonComponent } from './app/aphbutton/aphbutton.component';
 import { APHInputComponent } from './app/controls/aphinput/aphinput.component';
+import { APHCheckboxComponent } from './app/controls/aphcheckbox/aphcheckbox.component';
+import { APHCountrySelectComponent } from './app/controls/aphcountry-select/aphcountry-select.component';
+import { APHPhoneInputComponent } from './app/controls/aphphone-input/aphphone-input.component';
+import { APHRadioComponent } from './app/controls/aphradio/aphradio.component';
+import { APHSearchDropdownComponent } from './app/controls/aphsearch-dropdown/aphsearch-dropdown.component';
+import { APHSkeletonComponent } from './app/loaders/aphskeleton/aphskeleton.component';
 
 (async () => {
 
@@ -11,13 +17,20 @@ import { APHInputComponent } from './app/controls/aphinput/aphinput.component';
     ],
   });
 
-  const buttonElement = createCustomElement(APHButtonComponent, {
-    injector: app.injector
-  });
-  const inputElement = createCustomElement(APHInputComponent, {
-    injector: app.injector
-  });
-
-  customElements.define('aph-button', buttonElement);
-  customElements.define('aph-input', inputElement);
+  const components = [
+    { name: 'aph-button', component: APHButtonComponent },
+    { name: 'aph-input', component: APHInputComponent },
+    { name: 'aph-checkbox', component: APHCheckboxComponent },
+    { name: 'aph-country-select', component: APHCountrySelectComponent },
+    { name: 'aph-phone-input', component: APHPhoneInputComponent },
+    { name: 'aph-radio', component: APHRadioComponent },
+    { name: 'aph-search-dropdown', component: APHSearchDropdownComponent },
+    { name: 'aph-skeleton', component: APHSkeletonComponent },
+  ]
+  for (let item of components) {
+    const element = createCustomElement(item.component, {
+      injector: app.injector
+    });
+    customElements.define(item.name, element);
+  }
 })();
