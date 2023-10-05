6fb2b18bb9495e6ac94567a94ba10dc69e6826e9
Seiji Villafranca <seiji.ralphvillafranca@kpn.com>
2023-10-05 08:53:30 +0200

    Add modal and loader base component - add modal to create - add progress bar to create

---
+++ src/app/loaders/aphprogress-bar/aphprogress-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aphprogress-bar',
+  standalone: true,
+  template: ``,
+  styles: [
+    `
+    `
+  ]
+})
+export class APHProgressBarComponent {
+
+}
